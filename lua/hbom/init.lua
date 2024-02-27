68575ad0787fb30624f050cd2285e2620a09e117
Vladislav Panfilov <panfilovvv@uniweb.ru>
2024-02-27 23:38:02 +0500

    Init

---
+++ lua/hbom/init.lua
@@ -0,0 +1,10 @@
+require('hbom.lazy')
+
+require('hbom.lualine')
+--require('hbom.bufferline')
+require('hbom.neo-tree')
+require('hbom.telescope')
+require('hbom.lspconfig')
+--require('hbom.gitsigns')
+
+vim.cmd [[colorscheme tokyonight]]
---
+++ lua/settings.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+opt.fileformat = "unix"
+
+opt.termguicolors = true
