59eb47610d2a0a1b66023342844f90d24c19a5cf
Vladislav Panfilov <andromalak222@gmail.com>
2024-04-12 02:43:22 +0500

    lualine theme

--- lua/hbom/lazy/plugins/lualine.lua
+++ lua/hbom/lazy/plugins/lualine.lua
@@ -2,7 +2,11 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require('lualine').setup()
+		require('lualine').setup{
+			options = {
+				theme = 'iceberg_light',
+			},
+		}
 		require('transparent').clear_prefix('lualine')
 	end,
 }
