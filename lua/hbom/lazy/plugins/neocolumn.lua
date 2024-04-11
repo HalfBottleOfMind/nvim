32cbaeb644bf4863a50dd29eeaaf481cde9f064f
Vladislav Panfilov <andromalak222@gmail.com>
2024-04-12 04:05:22 +0500

    neocolumn

---
+++ lua/hbom/lazy/plugins/neocolumn.lua
@@ -0,0 +1,9 @@
+return {
+	'ecthelionvi/NeoColumn.nvim',
+	opts = {
+		always_on = true,
+		-- bg_color = "#FF0000",
+		fg_color = "#FF0080",
+	},
+	config = true
+}
--- lua/hbom/lazy/plugins/treesitter-context.lua
+++ lua/hbom/lazy/plugins/treesitter-context.lua
@@ -1,3 +1,6 @@
 return {
 	'nvim-treesitter/nvim-treesitter-context',
+	config = function ()
+		require('transparent').clear_prefix('TreesitterContext')
+	end
 }
