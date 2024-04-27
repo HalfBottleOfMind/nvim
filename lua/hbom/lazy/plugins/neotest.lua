43b6b0df7e6f1fc04a41f507b5622031fe363377
Vladislav Panfilov <andromalak222@gmail.com>
2024-04-27 08:06:09 +0500

    test change

--- lua/hbom/lazy/plugins/neotest.lua
+++ lua/hbom/lazy/plugins/neotest.lua
@@ -1,37 +1,37 @@
 return {
-	'nvim-neotest/neotest',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'antoinemadec/FixCursorHold.nvim',
-		'nvim-treesitter/nvim-treesitter',
-		'nvim-neotest/nvim-nio',
-
-		'nvim-neotest/neotest-go',
-		'halfbottleofmind/neotest-phpunit', -- TODO: change to original repository after PR merge
-	},
-	config = function()
-		local neotest_ns = vim.api.nvim_create_namespace('neotest')
-		vim.diagnostic.config({
-			virtual_text = {
-				format = function(diagnostic)
-					local message = diagnostic.message:gsub('\n', ' '):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-					return message
-				end,
-			},
-		}, neotest_ns)
-
-		require('neotest').setup({
-			adapters = {
-				require('neotest-go'),
-				require('neotest-phpunit')({
-					phpunit_cmd = './vendor/bin/phpunit',
-					filter_dirs = { 'vendor' },
-				}),
-			},
-		})
-
-		vim.keymap.set('n', '<leader>ts', function()
-			require('neotest').summary.toggle()
-		end)
-	end,
+	-- 'nvim-neotest/neotest',
+	-- dependencies = {
+	-- 	'nvim-lua/plenary.nvim',
+	-- 	'antoinemadec/FixCursorHold.nvim',
+	-- 	'nvim-treesitter/nvim-treesitter',
+	-- 	'nvim-neotest/nvim-nio',
+	--
+	-- 	'nvim-neotest/neotest-go',
+	-- 	'halfbottleofmind/neotest-phpunit', -- TODO: change to original repository after PR merge
+	-- },
+	-- config = function()
+	-- 	local neotest_ns = vim.api.nvim_create_namespace('neotest')
+	-- 	vim.diagnostic.config({
+	-- 		virtual_text = {
+	-- 			format = function(diagnostic)
+	-- 				local message = diagnostic.message:gsub('\n', ' '):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+	-- 				return message
+	-- 			end,
+	-- 		},
+	-- 	}, neotest_ns)
+	--
+	-- 	require('neotest').setup({
+	-- 		adapters = {
+	-- 			require('neotest-go'),
+	-- 			require('neotest-phpunit')({
+	-- 				phpunit_cmd = './vendor/bin/phpunit',
+	-- 				filter_dirs = { 'vendor' },
+	-- 			}),
+	-- 		},
+	-- 	})
+	--
+	-- 	vim.keymap.set('n', '<leader>ts', function()
+	-- 		require('neotest').summary.toggle()
+	-- 	end)
+	-- end,
 }
