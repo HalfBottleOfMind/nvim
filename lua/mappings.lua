a40aeb2711db009e777c36ce609bccce00ec4b00
Vladislav Panfilov <andromalak222@gmail.com>
2024-04-27 08:08:31 +0500

    add tmux-navigator

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,3 +6,9 @@
 vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end)
 vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end)
 vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end)
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
