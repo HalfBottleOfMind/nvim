9df71bb719de60a5708bd92e494b6b47e32680b4
Vladislav Panfilov <andromalak222@gmail.com>
2024-04-27 08:05:43 +0500

    neotree toggle bind

--- lua/hbom/lazy/plugins/neo-tree.lua
+++ lua/hbom/lazy/plugins/neo-tree.lua
@@ -1,11 +1,13 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
+	'nvim-neo-tree/neo-tree.nvim',
 	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons',
+		'MunifTanjim/nui.nvim',
 	},
 	config = function()
 		require('neo-tree').setup(require('hbom.lazy.plugins.configs.neo-tree'))
+
+		vim.keymap.set('n', '<leader>nt', ':Neotree toggle<CR>', {})
 	end,
 }
