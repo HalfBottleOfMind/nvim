375e262536226b61b9d1a388bd84ac43f26c4de5
Vladislav Panfilov <andromalak222@gmail.com>
2024-04-27 08:05:31 +0500

    add gitsigns bindings

--- lua/hbom/lazy/plugins/gitsigns.lua
+++ lua/hbom/lazy/plugins/gitsigns.lua
@@ -1,6 +1,10 @@
 return {
-	"lewis6991/gitsigns.nvim",
+	'lewis6991/gitsigns.nvim',
 	config = function()
 		require('gitsigns').setup()
+
+		vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', {})
+
+		vim.keymap.set('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', {})
 	end
 }
